aa422b95df93d31f53b77302b91ac202adb053c6
JAY <plasticvtree@gmail.com>
2025-03-26 09:32:33 +0100

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   /* config options here */
   output: 'export',
-  distDir: process.env.NODE_ENV === "production" ? "build" : ".next",
+  distDir: 'out',
   images: {
     unoptimized: true,
     domains: [
